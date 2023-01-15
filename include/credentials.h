362249e3ffd2b67ffac6ea14390e34548e0eebd0
Mattia Dalla Costa <matty7787878@gmail.com>
2023-01-15 12:01:01 +0100

    Removed my credentials

--- include/credentials.h
+++ include/credentials.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
-const char* ssid = "Mi 11";  // SSID WiFi network
-const char* pass = "00000000";  // Password  WiFi network
-const char* token = "5891807658:AAFgQDuxotPpZaP_a5bZ5FJj7XPrmKGByKo"; // token obtained from the botfather telegram bot (@BotFather)
+const char* ssid =  "---";  // SSID WiFi network
+const char* pass =  "---";  // Password  WiFi network
+const char* token = "---"; // token obtained from the botfather telegram bot (@BotFather)
