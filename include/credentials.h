e288e778897eea71419ff52a81e5f83e94b0213e
MattiaDallaCosta <matty7787878@gmail.com>
2023-01-15 12:01:28 +0100

    added secret credentials file

--- include/credentials.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-const char* ssid = "Mi 11";  // SSID WiFi network
-const char* pass = "00000000";  // Password  WiFi network
-const char* token = "5891807658:AAFgQDuxotPpZaP_a5bZ5FJj7XPrmKGByKo"; // token obtained from the botfather telegram bot (@BotFather)
